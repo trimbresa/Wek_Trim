b241769e7fed9931dc896da1326c378d25bb36ca
Jason Rukman <unclej@gmail.com>
2015-10-07 12:01:45 -0700

    Fix up crazy length in password and make migrations

--- webapp/models.py
+++ webapp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class AddNewUser(models.Model):
 	email     = models.EmailField()
 	name      = models.CharField(blank = False, max_length = 50, null = True)
-	password  = models.CharField(blank = False, max_length = 250232323, null = True)
-	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)	
+	password  = models.CharField(blank = False, max_length = 50, null = True)
+	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
 	updated   = models.DateTimeField(auto_now_add = False, auto_now = True)
 
 	def __unicode__ (self):
@@ -17,9 +17,9 @@ class AddResource(models.Model):
 	description      = models.CharField(blank = False, max_length = 300, null = True)
 	level  = models.CharField(blank = False, max_length = 999, null = True)
 	url  = models.CharField(blank = False, max_length = 999, null = True)
-	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)	
+	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
 	updated   = models.DateTimeField(auto_now_add = False, auto_now = True)
-                 
+
 
 	def __unicode__ (self):
 		return self.email
